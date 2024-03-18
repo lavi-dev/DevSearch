e688d6be8ab97b21fbad5fcca3ec4c551de4a879
Lohit <lohitsharma92@gmail.com>
2024-03-17 22:19:03 -0600

    Form update

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 
 
 # Create your views here.
 def projects(request):
-    return render(request,'projects/projects.html')
-def project(request,pk):
-    return render(request,'projects/single-projects.html')
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request,'projects/projects.html',context)
 
+
+def project(request,pk):
+    projectObj = Project.objects.get(id=pk)
+    # tags = projectObj.tags.all()
+    return render(request,'projects/single-projects.html',{'project':projectObj})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form':form}
+    return render(request,'projects/project-form.html',context)
